c01f7dcc5b7af38a3d9e041bef49695abe7e6c25
ismael <ismaelteixeira6@gmail.com>
2018-03-03 14:40:52 -0400

    aprendiendo a modificar las animaciones en ReactJS

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logoPlatzi from './platzi.png';
 import './App.css';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 class App extends Component {
+  state = {
+    logo: logo,
+  }
+  onClick(){
+    this.setState({
+      logo: logoPlatzi,
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <CSSTransitionGroup
+            transitionName = "fade"
+            transitionEnterTimeout = {1000}
+            transitionLeaveTimeout = {1000}
+          >
+            <img
+              key={this.state.logo} 
+              src={this.state.logo} 
+              className="App-logo" 
+              alt="logo"
+            />
+          </CSSTransitionGroup>
+          <h1 className="App-title">Hola mundo!</h1>
+          <button onClick={this.onClick.bind(this)}>Click me!</button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
